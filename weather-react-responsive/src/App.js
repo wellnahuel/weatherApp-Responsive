5d45620f124033216b9c3937972bce81ce738327
wellnahuel <nahuelcittadino@gmail.com>
2023-01-11 16:44:21 -0300

    styles of primary sections

--- weather-react-responsive/src/App.js
+++ weather-react-responsive/src/App.js
@@ -1,10 +1,30 @@
-
+import hotBackground from './assets/hot.jpg'
+import coldBackground from './assets/cold.jpg'
 
 function App() {
   return (
-   <div className="app">
-    <h1>Hola</h1>
-   </div>
+    <div className="app" style={{ backgroundImage: `url(${coldBackground})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type='text' name='city' placeholder='Enter name of the City...'></input>
+            <button>°F</button>
+          </div>
+
+
+          <div className="section section__temperature">
+            <div className='icon'>
+              <h3>London, GB</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="prueba"></img>
+              <h3>Cloudy</h3>
+            </div>
+            <div className='temperature'>
+              <h1>34 °C</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
