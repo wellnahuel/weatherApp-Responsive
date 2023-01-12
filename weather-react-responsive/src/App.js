1074c11df0dbece1b7bf98207b975615b7c4cb3a
wellnahuel <nahuelcittadino@gmail.com>
2023-01-12 19:08:43 -0300

    add cards , icons, and normalize data

--- weather-react-responsive/src/App.js
+++ weather-react-responsive/src/App.js
@@ -1,34 +1,59 @@
 import hotBackground from './assets/hot.jpg'
 import coldBackground from './assets/cold.jpg'
 import Details from './components/Details'
+import { useEffect, useState } from 'react';
+import { getFormattedWeatherData } from './weather';
 
 function App() {
+
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData("paris", units) /* en weather.js a esta funcion se le pasa city y units */
+      /*   console.log(data); */
+      setWeather(data)
+    };
+    fetchWeatherData();
+  }, [])
+
   return (
     <div className="app" style={{ backgroundImage: `url(${coldBackground})` }}>
       <div className="overlay">
-        <div className="container">
-          <div className="section section__inputs">
-            <input type='text' name='city' placeholder='Enter name of the City...'></input>
-            <button>°F</button>
-          </div>
+        {/* quiero renderizar container solo si esta weather, sino no */}
+        {
+          weather && (
+            <div className="container">
+              <div className="section section__inputs">
+                <input type='text' name='city' placeholder='Enter name of the City...'></input>
+                <button>°F</button>
+              </div>
 
 
-          <div className="section section__temperature">
-            <div className='icon'>
-              <h3>London, GB</h3>
-              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="prueba"></img>
-              <h3>Cloudy</h3>
+              <div className="section section__temperature">
+                <div className='icon'>
+                  <h3>{`${weather.name}, ${weather.country}`}</h3>
+                  <img src={weather.iconURL} alt="prueba"></img>
+                  <h3>{weather.description}</h3>
+                </div>
+                <div className='temperature'>
+                  <h1>{`${weather.temp.toFixed()} ${units === "metric" ? "°C" : "°F"
+                    }`}</h1> {/* si la unidad es igual a metric mostrame Celsius, sino Farenheit */}
+                </div>
+              </div>
+
+              {/* ${units === "metric" ? "C" : "F"
+                    } */}
+
+
+              {/* bottom details */}
+
+              <Details weather={weather} units={units}/>
             </div>
-            <div className='temperature'>
-              <h1>34 °C</h1>
-            </div>
-          </div>
+          )
+        }
 
-
-        {/* bottom details */}
-
-        <Details />
-        </div>
       </div>
     </div>
   );
--- weather-react-responsive/src/components/Details.jsx
+++ weather-react-responsive/src/components/Details.jsx
@@ -1,45 +1,75 @@
 import React from 'react'
-import { TiArrowDownOutline } from 'react-icons/ti'
 import "./details.css"
 
-const Details = () => {
+import { FaArrowUp, FaArrowDown, FaWind } from "react-icons/fa";
+import { BiHappy } from "react-icons/bi";
+import { MdCompress, MdOutlineWaterDrop } from "react-icons/md";
+
+
+const Details = ({ weather, units }) => {
+
+    const tempUnit = units === "metric" ? "°C" : "°F";
+    const windUnit = units === "metric" ? "m/s" : "m/h";
+
+    const cards = [
+        {
+          id: 1,
+          icon: <FaArrowDown />,
+          title: "min",
+          data: weather.temp_min.toFixed(),
+          unit: tempUnit,
+        },
+        {
+          id: 2,
+          icon: <FaArrowUp />,
+          title: "max",
+          data: weather.temp_max.toFixed(),
+          unit: tempUnit,
+        },
+        {
+          id: 3,
+          icon: <BiHappy />,
+          title: "feels like",
+          data: weather.feels_like.toFixed(),
+          unit: tempUnit,
+        },
+        {
+          id: 4,
+          icon: <MdCompress />,
+          title: "pressure",
+          data: weather.pressure,
+          unit: "hPa",
+        },
+        {
+          id: 5,
+          icon: <MdOutlineWaterDrop />,
+          title: "humidity",
+          data: weather.humidity,
+          unit: "%",
+        },
+        {
+          id: 6,
+          icon: <FaWind />,
+          title: "wind speed",
+          data: weather.speed.toFixed(),
+          unit: windUnit,
+        },
+      ];
+
+
     return (
-        <div className='section section__details'>
-            <div className='card'>
-                <div className='details__card-icon'>
-                    <TiArrowDownOutline />
-                    <small>min</small>
+        <div className="section section__details">
+            {cards.map(({ id, icon, title, data, unit }) => (
+                <div key={id} className="card">
+                    <div className="details__card-icon">
+                        {icon}
+                        <small>{title}</small>
+                    </div>
+                    <h2>{`${data} ${unit}`}</h2>
                 </div>
-                <h2>33 °C</h2>
-            </div>
-
-            <div className='card'>
-                <div className='details__card-icon'>
-                    <TiArrowDownOutline />
-                    <small>min</small>
-                </div>
-                <h2>33 °C</h2>
-            </div>
-
-            <div className='card'>
-                <div className='details__card-icon'>
-                    <TiArrowDownOutline />
-                    <small>min</small>
-                </div>
-                <h2>33 °C</h2>
-            </div>
-
-            <div className='card'>
-                <div className='details__card-icon'>
-                    <TiArrowDownOutline />
-                    <small>min</small>
-                </div>
-                <h2>33 °C</h2>
-            </div>
-
-        
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Details
+export default Details;
